03c6f4a7378e11a58ba9fd5c2944e8c7c9723843
WShe996 <wshepelak2003@gmail.com>
2022-12-06 18:21:41 -0600

    finished the check_win() method

--- game_board.h
+++ game_board.h
@@ -12,8 +12,13 @@ class game_board
         }
 
 
+        /// @brief Returns true if the game has reached an end state
+        /// @return true if game is at end state, false if game is not
         bool check_win();
 
+
+        /// @brief Returns score value of the end game
+        /// @return Returns 1 if X has won the game -1 if O has won the game and 0 if it is a draw 
         int calc_end_game();
 
 
@@ -32,7 +37,27 @@ class game_board
 
 bool game_board::check_win()
 {
-    if()
+    if((board_vector[0][0]+board_vector[1][1]+board_vector[2][2]==3)||(board_vector[0][0]+board_vector[1][1]+board_vector[2][2]==-3))
+    {
+        return true;
+    }
+    else if((board_vector[2][0]+board_vector[1][1]+board_vector[0][2]==3)||(board_vector[2][0]+board_vector[1][1]+board_vector[0][2]==-3))
+    {
+        return true;
+    }
+    for(int i=0;i<3;i++)
+    {
+        if((board_vector[0][i]+board_vector[1][i]+board_vector[2][i]==3)||(board_vector[0][i]+board_vector[1][i]+board_vector[2][i]==-3)){return true;}
+        if((board_vector[i][0]+board_vector[i][1]+board_vector[i][2]==3)||(board_vector[i][0]+board_vector[i][1]+board_vector[i][2]==-3)){return true;}
+    }
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            if(board_vector[i][j]==0){return false;}
+        }
+    }
+    return true;
 }
 
 
