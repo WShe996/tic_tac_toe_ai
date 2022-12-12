ceb1470ab53b1b223167ccde53e813323e64e477
Carmen Thom <thomrya22@outlook.com>
2022-12-11 22:00:48 -0600

    Refactoring of code

--- game_board.h
+++ game_board.h
@@ -3,120 +3,115 @@
 #include <iostream>
 using namespace std;
 
-class game_board
-{
+class game_board {
     public:
-        game_board(){
-            board_vector = {{0,0,0}, {0,0,0}, {0,0,0}};
-            turn = true;
-        }
-
+        game_board() : board_vector{{0, 0, 0}, {0, 0, 0}, {0, 0, 0}}, turn(true) {}
 
         /// @brief Returns true if the game has reached an end state
         /// @return true if game is at end state, false if game is not
         bool check_win();
 
-
         /// @brief Returns score value of the end game
         /// @return Returns 1 if X has won the game -1 if O has won the game and 0 if it is a draw 
         int calc_end_game();
-
 
         /// @brief Returns true if move is played, false if it cant be played 
         /// @param  2d point x comp
         /// @param  2d point y comp
         /// @return 
-        bool play_move(int, int); 
+        bool can_move(int, int); 
 
         void print_board();
 
-        vector<int> get_game_vector();
+        std::vector<int> get_game_vector();
 
     private:
-        vector<vector<int>> board_vector = {{}};
+        std::vector<std::vector<int>> board_vector;
         bool turn;
-
 };
 
 
 int game_board::calc_end_game()
 {
-    if(check_win())
-    {
-        for(int i=0;i<3;i++)
-        {
-            int sum_x = (board_vector[0][i] + board_vector[1][i] + board_vector[2][i]);
-            int sum_y = (board_vector[i][0] + board_vector[i][1] + board_vector[i][2]);
-            if(sum_y * sum_y == 9){return sum_y/3;}
-            else if(sum_x * sum_x == 9){return sum_x/3;}
-        }
-        int diag_positive = board_vector[0][2] + board_vector[1][1] + board_vector[2][0];
-        int diag_negitive = board_vector[0][0] + board_vector[1][1] + board_vector [2][2];
-        if(diag_positive * diag_positive == 9){return diag_positive/3;}
-        else if(diag_negitive * diag_negitive == 9){return diag_negitive/3;}
-        else{return 0;}
-    }
-    else
+    if(!check_win()) 
     {
         throw std::invalid_argument("This is not an endgame board");
     }
+    
+    for(int i=0;i<3;i++)
+    {
+        int sum_x = (board_vector[0][i] + board_vector[1][i] + board_vector[2][i]);
+        int sum_y = (board_vector[i][0] + board_vector[i][1] + board_vector[i][2]);
+        if(sum_y * sum_y == 9) return sum_y/3;
+        else if(sum_x * sum_x == 9) return sum_x/3;
+    }
+    int diag_positive = board_vector[0][2] + board_vector[1][1] + board_vector[2][0];
+    int diag_negitive = board_vector[0][0] + board_vector[1][1] + board_vector [2][2];
+    if(diag_positive * diag_positive == 9) return diag_positive/3;
+    else if(diag_negitive * diag_negitive == 9) return diag_negitive/3;
+    else return 0;
 }
 
 bool game_board::check_win()
 {
-    int player_multiplyer = 0;
-    if(turn){player_multiplyer=-1;}
-    else{player_multiplyer=1;}
-    if(board_vector[0][0]+board_vector[1][1]+board_vector[2][2]== player_multiplyer*3)
-    {
+    int player_multiplyer = turn ? -1 : 1;
+    
+    if (board_vector[0][0] + board_vector[1][1] + board_vector[2][2] == player_multiplyer * 3 || 
+        board_vector[2][0] + board_vector[1][1] + board_vector[0][2] ==  player_multiplyer * 3) {
         return true;
     }
-    else if(board_vector[2][0]+board_vector[1][1]+board_vector[0][2]==  player_multiplyer*3)
-    {
-        return true;
+      
+    for (int i = 0; i < 3; i++) {
+        if (board_vector[0][i] + board_vector[1][i] + board_vector[2][i] == player_multiplyer * 3 || 
+            board_vector[i][0] + board_vector[i][1] + board_vector[i][2] == player_multiplyer * 3) {
+            return true;
+        }
     }
-    for(int i=0;i<3;i++)
-    {
-        if(board_vector[0][i]+board_vector[1][i]+board_vector[2][i]== player_multiplyer*3){return true;}
-        else if(board_vector[i][0]+board_vector[i][1]+board_vector[i][2]== player_multiplyer*3){return true;}
-    }
-    for(int i=0;i<3;i++)
-    {
-        for(int j=0;j<3;j++)
-        {
-            if(board_vector[i][j]==0){return false;}
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            if (board_vector[i][j] == 0) {
+                return false;
+            }
         }
     }
     return true;
 }
 
 
-bool game_board::play_move(int x, int y)
-{
-    if((x>=3)||(y>=3)){return false;}
-    if(this->board_vector[x][y]!=0){return false;}
-    else{
-        if(this->turn) //X turn
-        {
-            this->board_vector[x][y]=1;
-        }
-        else { //Y turn
-            this->board_vector[x][y]=-1;
-        }
-        this->turn = !this->turn;
+bool game_board::can_move(int x, int y) {
+    if ((x >= 3) || (y >= 3)) {
+        return false;
+    }
+    if (board_vector[x][y] != 0) {
+        return false;
+    } else {
+        board_vector[x][y] = this->turn ? 1 : -1;
+       turn = !this->turn;
         return true;
     }
 }
 
 void game_board::print_board()
 {
-    for(int i=0;i<3;i++)
+    for(int i=0; i < 3; i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0; j < 3; j++)
         {
-            if(board_vector[i][j] == 1){cout << "X";}
-            else if(board_vector[i][j] == 0){cout << " ";}
-            else{cout << "O";}
+            char marker;
+            if (board_vector[i][j] == 1)
+            {
+                marker = 'X';
+            }
+            else if (board_vector[i][j] == 0)
+            {
+                marker = ' ';
+            }
+            else
+            {
+                marker = 'O';
+            }
+            cout << marker;
         }
         cout << "\n";
     }
@@ -125,9 +120,9 @@ void game_board::print_board()
 vector<int> game_board::get_game_vector()
 {
     vector<int> nums;
-    for(int i=0;i<3;i++)
+    for(int i=0; i < 3; i++)
     {
-        for(int j=0;j<3;j++)
+        for(int j=0; j < 3; j++)
         {
             nums.push_back(board_vector[i][j]);
         }
--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 /*
-int main()
+int test()
 {
 	model my_model = model();
 	my_model.load("model2.txt");
@@ -20,83 +20,95 @@ int main()
 */
 
 
-void data_parce(vector<vector<double>> & x_dat, vector<vector<double>> & y_dat)
+void parse_data(vector<vector<double>>& x_dat, vector<vector<double>>& y_dat)
 {
 	fstream _file("data.csv", std::fstream::in);
 	string str = "";
 	vector<double> temp;
-	while(getline(_file, str))
+	while (getline(_file, str))
 	{
-		while(str.find(",")!=string::npos)
+		while (str.find(",") != string::npos)
 		{
-			int num = stod(str.substr(0,str.find(",")));
+			int num = stod(str.substr(0, str.find(",")));
 			temp.push_back(num);
-			str.erase(0,str.find(",")+1);
-			if(str==","){break;}
+			str.erase(0, str.find(",") + 1);
+			if (str == ",") { break; }
 		}
-		y_dat.push_back({temp[temp.size()-1]});
+		y_dat.push_back({ temp[temp.size() - 1] });
 		temp.pop_back();
 		x_dat.push_back(temp);
 		temp.clear();
 	}
 }
-int main()
+
+int create_data()
 {
+	fstream file("data.csv", std::fstream::in | std::fstream::app);
+	int num = 10000; //change for amount of data points needed
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dis(0,2);
+
+	for(int i=0;i<num;i++) {
+		vector<vector<int>> gen_data;
+		game_board my_game = game_board();
+
+		while(!my_game.check_win())
+		{
+			int a = dis(gen);
+			int b = dis(gen);
+			if(my_game.can_move(a,b))
+			{
+				gen_data.push_back(my_game.get_game_vector());
+			} 
+		}
+
+		int score = my_game.calc_end_game();
+
+		for(int j=0;j<gen_data.size();j++)
+		{
+			gen_data[j].push_back(score);
+		}
+
+		for(int j=0;j<gen_data.size();j++)
+		{
+			for(int k=0;k<gen_data[0].size();k++)
+			{
+				file << gen_data[j][k];
+				file << ",";
+			}
+			file << "\n";
+		}
+	}
+	file.close();
+	return(0);
+}
+
+int train_data() {
 	vector<vector<double>> x_data;
 	vector<vector<double>> y_data;
-	data_parce(x_data, y_data);
+	parse_data(x_data, y_data);
+	
 	model my_model(9, MSE, 0.02);
 	my_model.add_hidden_layer(tanH, 9);
 	my_model.add_hidden_layer(tanH, 16);
 	my_model.add_hidden_layer(tanH, 9);
 	my_model.add_hidden_layer(tanH, 1);
+	
 	my_model.train(x_data, y_data, 50, false);
 	my_model.save("model2.txt");
-	vector<double> number = my_model.solve_model({1,1,0,0,0,-1,0,0,-1});
-	cout << number[0] << endl;
-	number = my_model.solve_model({1,1,-1,0,0,0,0,0,-1});
-	cout << number[0] << endl; 
+	
+	vector<double> solution1 = my_model.solve_model({1,1,0,0,0,-1,0,0,-1});
+	vector<double> solution2 = my_model.solve_model({1,1,-1,0,0,0,0,0,-1});
+	
+	cout << solution1[0] << endl;
+	cout << solution2[0] << endl; 
+	
 	return 0;
 }
 
-
-
-/*
 int main()
 {
-	fstream _file("data.csv", std::fstream::in | std::fstream::app);
-	int num = 10000; //change for amount of data points needed
-	random_device rd;
-	mt19937 gen(rd());
-	uniform_int_distribution<> dis(0,2);
-	for(int i=0;i<num;i++) {
-		vector<vector<int>> gen_data;
-		game_board my_game = game_board();
-		while(!my_game.check_win())
-		{
-
-			int a, b;
-			a = dis(gen);
-			b = dis(gen);
-			if(my_game.play_move(a,b))
-			{
-				gen_data.push_back(my_game.get_game_vector());
-			} 
-		}
-		int score = my_game.calc_end_game();
-		for(int j=0;j<gen_data.size();j++){gen_data[j].push_back(score);}
-		for(int j=0;j<gen_data.size();j++)
-		{
-			for(int k=0;k<gen_data[0].size();k++)
-			{
-				_file << gen_data[j][k];
-				_file << ",";
-			}
-			_file << "\n";
-		}
-	}
-	_file.close();
-	return(0);
+	create_data();
+	train_data();
 }
-
-*/
